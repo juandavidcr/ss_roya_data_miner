19f75ea98e4feba26617b2f59973639aa940cfc2
juan_david_cr <j1dcrane@gmail.com>
2022-07-27 23:55:32 -0500

    Limpieza de data

--- script2Data.py
+++ script2Data.py
@@ -1,13 +1,12 @@
 import re
 from datetime import date
 
-
+#Buscamos en el archivo de cabaceras en este caso newfile.txt y encontrar patrones de escritura y cuantas palabras hay en una linea
 filename = './newfile.txt'
 with open(filename) as file_obj:
     # for line in file_obj:
     #     print(line.rstrip())
     lines = file_obj.readlines()
-    #print(lines)
     
 for line in lines:
     listaPalabras = line.split()
@@ -16,7 +15,7 @@ for line in lines:
     for w in listaPalabras:
         frecuenciaPalab.append(listaPalabras.count(w))
 
-    print("Cadena\n" + line +"\n")
+    #print("Cadena\n" + line +"\n")
     print("Lista\n" + str(listaPalabras) + "\n")
     print("Frecuencias\n" + str(frecuenciaPalab) + "\n")
     print("Pares\n" + str(list(zip(listaPalabras, frecuenciaPalab))))
