1fcdaef01e0bab1556b09f40301ffc194fa8ae5a
juan_david_cr <j1dcrane@gmail.com>
2022-08-10 00:44:36 -0500

    Se realizan las listas de cada columna a ingestar en la tabla de Datos_Climatologicos, falta añadirle el id de la estacion dependiendo del municipio

---
+++ scriptForCSV.py
@@ -0,0 +1,51 @@
+import os
+import re
+import itertools
+
+patternDeFechas = '^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$'
+patternDePrecip = '[0-9]*\.?[0-9]*'
+patternDeEvap = '(\w*)|([0-9]*\.?[0-9]*)'
+
+listaDeFechas = []
+listaPrecipitaciones=[]
+listaEvaporacion=[]
+listTmax=[]
+listTmin=[]
+
+archivo = open("./coatepecdata.txt")
+#archivo = open("./brionesdata.txt")
+for linea in archivo:
+    linea = linea.rstrip("\\s") 
+    listResult=re.split(r'\s+', linea)
+    print(listResult)
+    resultFecha = re.match(patternDeFechas, listResult[0])
+    resultPrecipitacion=re.match(patternDePrecip, listResult[1])
+    resultEvaporacion=re.match(patternDePrecip, listResult[2])
+    if resultFecha:
+        #print("Search successful.")
+        listaDeFechas.append(listResult[0])
+        
+    else:
+        print("Search unsuccessful.")
+    if resultPrecipitacion:
+        #print("Search successful.")
+        listaPrecipitaciones.append(listResult[1])
+        
+    else:
+        print("Search unsuccessful.")
+    if resultEvaporacion:
+        #print("Search successful.")
+        listaEvaporacion.append(listResult[2])
+        
+    else:
+        print("Search unsuccessful.")
+print(listaDeFechas,len(listaDeFechas))
+print(listaPrecipitaciones,len(listaDeFechas))
+print(listaEvaporacion,len(listaDeFechas))
+        #print(listResult[0])
+
+# archivo2 = open("briones.csv","a")
+# for line in itertools.islice(archivo, 1, len(listResult)):
+#     archivo2.writelines(line)
+# archivo2.close()
+# archivo.close()
