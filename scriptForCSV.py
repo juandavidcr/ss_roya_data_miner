841a0a19a246e799d24a1d32c81b259b11838170
juan_david_cr <j1dcrane@gmail.com>
2022-08-24 19:58:10 -0500

    Tratamiento de fecha y archivo para construccion de importacion

--- scriptForCSV.py
+++ scriptForCSV.py
@@ -1,6 +1,12 @@
-import os
 import re
-import itertools
+from datetime import date
+
+
+
+def createFecha(aaaa,mm,dd):
+    fecha = date(int(aaaa),int(mm),int(dd))
+    #print(fecha)
+    return fecha
 
 patternDeFechas = '^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$'
 patternDePrecip = '[0-9]*\.?[0-9]*'
@@ -12,52 +18,46 @@ listaPrecipitaciones=[]
 listaEvaporacion=[]
 listTmax=[]
 listTmin=[]
-
+nuevaLista=[]
 archivo = open("./coatepecdata.txt")
 #archivo = open("./brionesdata.txt")
 for linea in archivo:
     linea = linea.rstrip("\\s") 
     listResult=re.split(r'\s+', linea)
-    print(listResult)
+    #print(listResult)
     resultFecha = re.match(patternDeFechas, listResult[0])
     resultPrecipitacion=re.match(patternDePrecip, listResult[1])
     resultEvaporacion=re.match(patternDePrecip, listResult[2])
     resultTmax=re.match(patternTmaxyTmin, listResult[3])
     resultTmin=re.match(patternTmaxyTmin, listResult[4])
     if resultFecha:
-        #print("Search successful.")
         listaDeFechas.append(listResult[0])        
-    else:
-        print("Search unsuccessful.")
     if resultPrecipitacion:
-        #print("Search successful.")
         listaPrecipitaciones.append(listResult[1])        
-    else:
-        print("Search unsuccessful.")
     if resultEvaporacion:
-        #print("Search successful.")
         listaEvaporacion.append(listResult[2])
-    else:
-        print("Search unsuccessful.")
     if resultTmax:
-        #print("Search successful.")
         listTmax.append(listResult[3])
-    else:
-        print("Search unsuccessful.")
     if resultTmin:
-        #print("Search successful.")
         listTmin.append(listResult[3])
-    else:
-        print("Search unsuccessful.")    
-print(listaDeFechas,len(listaDeFechas))
-print(listaPrecipitaciones,len(listaPrecipitaciones))
-print(listaEvaporacion,len(listaEvaporacion))
-print(listTmax,len(listTmax))
-print(listTmin,len(listTmin))
-        #print(listResult[0])
-
-# archivo2 = open("briones.csv","a")
-# for line in itertools.islice(archivo, 1, len(listResult)):
-#     archivo2.writelines(line)
-# archivo2.close()
-# archivo.close()
+#print(listaDeFechas,len(listaDeFechas))
+#print(listaPrecipitaciones,len(listaPrecipitaciones))
+#print(listaEvaporacion,len(listaEvaporacion))
+#print(listTmax,len(listTmax))
+#print(listTmin,len(listTmin))
+#print(type(listaDeFechas))
+strFecha = str(listaDeFechas[0])
+#print(strFecha)
+#print(type(strFecha))
+nuevaLista =strFecha.split('/')
+print()
+dia=nuevaLista[0]
+mes=nuevaLista[1]
+anyo=nuevaLista[2]
+fechaa = createFecha(anyo,mes,dia)
+#INSERT INTO `climatologia_diaria`.`Datos_Climatologicos` (`id_climatologicos`, `fecha`, `precipitacion_mm`, `evaporacion_mm`, `tmax`, `tmin`, `humedad_relativa`, `estacion_id`) VALUES ('2', '1999-01-01', '0.0', '', '12.0', '11.0', '', '1');
+print(f"INSERT INTO Datos_Climatologicos (fecha, precipitacion_mm,evaporacion_mm,tmax,tmin,humedad_relativa,estacion_id) VALUES('{fechaa}',")
+print("""
+INSERT INTO `climatologia_diaria`.`Datos_Climatologicos` ( `fecha`, `precipitacion_mm`, `evaporacion_mm`, `tmax`, `tmin`, `humedad_relativa`, `estacion_id`) 
+VALUES ( '"+fechaa+"')
+""")
